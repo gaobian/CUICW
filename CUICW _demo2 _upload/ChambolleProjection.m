f665e4e9d29d8271b3ab47ced8b643648d9a02d8
gb <96167585+gaobian@users.noreply.github.com>
2024-03-23 00:03:14 +0800

    Update ChambolleProjection.m

--- CUICW _demo2 _upload/ChambolleProjection.m
+++ CUICW _demo2 _upload/ChambolleProjection.m
@@ -1,18 +1,16 @@
 % chambolle projection algorithm
-% input image u0
-% g MxN, p MxNx2 
 function u=ChambolleProjection(g,tao,lambda,n)
 % Chambolle Projection uses chambolle projection algorithm denoising .
 % u0 input image
 % n iteration time
 [M,N,~]=size(g);
-p=zeros(M,N,2); % initialize p0
+p=zeros(M,N,2); 
 for i=1:n
     X=div(p)-g/lambda;
     gradX=grad(X);
     numerator=p+tao*gradX;
     denominator=1+tao*sqrt(gradX(:,:,1).^2+gradX(:,:,2).^2);
-    p=numerator./denominator; % MxNx2维的矩阵是可以和MxNx1的矩阵进行点乘和点除操作�?
+    p=numerator./denominator; 
     
 end
 
